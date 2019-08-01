a499f846e598c6e0f02e5b1a6d0fddce6f4569d3
ekwayv8 <yvon.ekwalla@gmail.com>
2019-08-01 13:54:50 +0200

    Réalisation de 1/2 du 1er exo

---
+++ FreeDoc/db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+#Boucles permettant de créer de faux objets afin de tester notre programme 
+require 'faker'
+100.times do
+
+	patient_list = Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+	doctor_list = Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name ,specialty: Faker::Name.first_name, zip_code: Faker::Address.zip_code)
+	appointment_list =Appointment.create!(date: Faker::Date.forward, doctor: doctor_list, patient: patient_list)
+
+end
